e1020685eafd6e2b5704fc454786d9a1b953e18c
Ada <adrianna.kucinska@onet.eu>
2017-07-08 10:53:39 +0200

    File without wxyz

--- code_machine.rb
+++ code_machine.rb
@@ -1,31 +1,45 @@
 def encrypt_with_caesar(original_message, shift = 5)
   alphabet = Array('a'..'z')
   encrypted_message = ""
+
   original_message.downcase.split('').each do |letter|
+
     if letter == " "
       encrypted_message += " "
+
     else
       new_letter_position = alphabet.find_index(letter) + shift
       new_letter = alphabet.at(new_letter_position) % 26
       encrypted_message += new_letter
+      #27 % 26 = 1
     end
+
   end
-  puts encrypted_message
-  rescue
-  puts "Uzywaj tylko liter"
+  
+  return encrypted_message  
+
+rescue
+  return "Uzywaj tylko liter"
+
 end
+
 puts "Podaj tekst do zaszyfrowania:"
-original_message=gets.chomp
-original_message.downcase
-encrypt_with_caesar(original_message)
+original_message = gets.chomp
+puts encrypt_with_caesar(original_message)
+
 puts "Szyfrujemy dalej? (tak/nie)"
-text=gets.chomp
+text = gets.chomp
+
   if text == "nie"
     puts "Koniec szyfrowania"
-  else
+
+  elsif     
     text == "tak"
     puts "Podaj tekst do zaszyfrowania:"
     original_message = gets.chomp
-    encrypt_with_caesar(original_message)
-  end 
+    puts encrypt_with_caesar(original_message)
 
+  else
+    puts "Użyj 'tak' lub 'nie'!!!"
+    text = gets.chomp
+  end
