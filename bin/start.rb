8e32f317c76ced892900e0902a849695b584ad23
Ada <adrianna.kucinska@onet.eu>
2017-07-08 13:22:29 +0200

    Add file start.rb with loop

--- bin/start.rb
+++ bin/start.rb
@@ -1,20 +1,15 @@
 #!/usr/bin/env ruby
 require './code_machine.rb'
 
-puts "Podaj tekst do zaszyfrowania:"
-original_message = gets.chomp
-puts encrypt_with_caesar(original_message)
+text = "tak"
 
-puts "Szyfrujemy dalej? (tak/nie)"
-text = gets.chomp
-
-if text == "nie"
-  puts "Koniec szyfrowania"
-elsif text == "tak"
+while text == "tak"
   puts "Podaj tekst do zaszyfrowania:"
   original_message = gets.chomp
   puts encrypt_with_caesar(original_message)
-else
-  puts "Użyj 'tak' lub 'nie'!!!"
+
+  puts "Szyfrujemy dalej? (tak/nie)"
   text = gets.chomp
 end
+
+puts "Koniec szyfrowania"
